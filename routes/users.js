564ac8aee41416fe808775a4b563463a30d431a6
baozi <baozi5342@gmail.com>
2026-01-19 04:00:48 +0800

    新版本

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const db = require("../config/db")
+;
 
 /* 用户列表 */
 router.get("/", async (req, res) => {
--- routes/withdraw.js
+++ routes/withdraw.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const db = require("../config/db")
+;
 
 /* 提现列表 */
 router.get("/", async (req, res) => {
